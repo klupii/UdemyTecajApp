c9ea00bfb35ca153d5f2b834d6472064fa19262d
Klara <klara.lupoglavac7@gmail.com>
2025-07-30 20:28:07 +0200

    project init

---
+++ server/models/Product.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    images: {
+        type: Array,
+        default: [],
+        required: true,
+    },
+    brand: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    reviews: {
+        type: Array,
+        default: [],
+        required: true,
+    },
+    rating: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    numberOfReviews: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    stock: {
+        type: Number,
+        required: true,
+    },
+    productIsNew: {
+        type: Boolean,
+        required: true,
+    },
+    stripeId: {
+        type: String,
+    },
+} , {timestamps: true});
+
+    const Product = mongoose.model("Product", productSchema);
+    export default Product;
